6a658795666b55dd9a48aa3a395cda4f6cb55347
DamainBL <dbermudezl@unal.edu.co>
2025-10-28 15:52:07 -0500

    Delete backend firebase directory

--- backend firebase/main.py
+++
@@ -1,8 +0,0 @@
-from ui.sistema import Sistema
-
-def main():
-    sistema = Sistema()
-    sistema.menu()
-
-if __name__ == "__main__":
-    main()
--- backend firebase/models/persona.py
+++
@@ -1,6 +0,0 @@
-class Persona:
-    def __init__(self, usuario,  correo, contraseña):
-        self.usuario = usuario
-        self.correo = correo
-        self.contraseña = contraseña
-
